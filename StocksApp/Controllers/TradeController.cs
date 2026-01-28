fad295825451fe0cacc0462c3bb5ec49a085e9e4
João Felipe Fernandes Pimentel <joaofelipefernandes3221@gmail.com>
2026-01-28 20:37:43 -0300

    feat(controller) add validation and default values

--- StocksApp/Controllers/TradeController.cs
+++ StocksApp/Controllers/TradeController.cs
@@ -8,31 +8,41 @@ namespace StocksApp.Controllers
 {
     public class TradeController : Controller
     {
-        private readonly IOptions<TradingOptions> _tradingOptions;
+        private readonly TradingOptions _tradingOptions;
         private readonly IFinnhubService _finnhubService;
         private readonly IConfiguration _configuration;
 
         public TradeController(IOptions<TradingOptions> tradingOptions, IFinnhubService finnhubService, IConfiguration configuration)
         {
-            _tradingOptions = tradingOptions;
+            _tradingOptions = tradingOptions.Value;
             _finnhubService = finnhubService;
             _configuration = configuration;
         }
 
         [Route("/")]
+        [Route("[action]")]
+        [Route("~/[controller]")]
         public async Task<IActionResult> Index()
         {
-            string defaultStockSymbol = _tradingOptions.Value.DefaultStockSymbol;
-            var companyProfile = await _finnhubService.GetCompanyProfile(defaultStockSymbol);
-            var stockPriceQuote = await _finnhubService.GetStockPriceQuote(defaultStockSymbol);
+            if (string.IsNullOrEmpty(_tradingOptions.DefaultStockSymbol))
+                _tradingOptions.DefaultStockSymbol = "MSFT";
 
-            StockTrade stockTrade = new StockTrade
+            string? defaultStockSymbol = _tradingOptions.DefaultStockSymbol;
+            var companyProfile = await _finnhubService.GetCompanyProfile(defaultStockSymbol!);
+            var stockPriceQuote = await _finnhubService.GetStockPriceQuote(defaultStockSymbol!);
+
+            StockTrade stockTrade = new StockTrade() { StockSymbol = defaultStockSymbol };
+
+            if (companyProfile != null && stockPriceQuote != null)
             {
-                StockSymbol = defaultStockSymbol,
-                StockName = companyProfile != null && companyProfile.ContainsKey("name") ? ((JsonElement)companyProfile["name"]).GetString() : "N/A",
-                Price = stockPriceQuote != null && stockPriceQuote.ContainsKey("c") ? ((JsonElement)stockPriceQuote["c"]).GetDouble() : 0.0,
-                Quantity = 0
-            };
+                stockTrade = new StockTrade()
+                {
+                    StockSymbol = defaultStockSymbol,
+                    StockName = companyProfile != null && companyProfile.ContainsKey("name") ? ((JsonElement)companyProfile["name"]).GetString() : "N/A",
+                    Price = stockPriceQuote != null && stockPriceQuote.ContainsKey("c") ? ((JsonElement)stockPriceQuote["c"]).GetDouble() : 0.0,
+                    Quantity = 0
+                };
+            }
 
             ViewBag.ApiKey = _configuration["apiKey"];
 
--- StocksApp/ViewModels/StockTrade.cs
+++ StocksApp/ViewModels/StockTrade.cs
@@ -4,7 +4,7 @@
     {
         public string? StockSymbol { get; set; }
         public string? StockName { get; set; }
-        public double Price { get; set; }
-        public int Quantity { get; set; }
+        public double Price { get; set; } = 0;
+        public uint Quantity { get; set; } = 0;
     }
 }
