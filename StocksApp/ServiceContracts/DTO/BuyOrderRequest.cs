42c7caf2151aac23632241c03b4b8edaba3df9b3
João Felipe Fernandes Pimentel <joaofelipefernandes3221@gmail.com>
2026-01-30 15:55:09 -0300

    feat: Add all BuyOrders and SellOrders request/response DTOs

---
+++ StocksApp/ServiceContracts/DTO/BuyOrderRequest.cs
@@ -0,0 +1,24 @@
+﻿using StocksApp.Entities;
+using StocksApp.ServiceContracts.CustomValidators;
+using System.ComponentModel.DataAnnotations;
+
+namespace StocksApp.ServiceContracts.DTO
+{
+    public class BuyOrderRequest
+    {
+        [Required(ErrorMessage = "StockSymbol can't be empty")]
+        public string? StockSymbol { get; set; }
+
+        [Required(ErrorMessage = "StockName can't be empty")]
+        public string? StockName { get; set; }
+
+        [MinimumDate]
+        public DateTime DateAndTimeOfOrder { get; set; }
+
+        [Range(1, 100000, ErrorMessage = "Quantity must be 1 to 100000")]
+        public uint Quantity { get; set; }
+
+        [Range(1, 10000, ErrorMessage = "Price must be 1 to 10000")]
+        public double Price { get; set; }
+    }
+}
---
+++ StocksApp/ServiceContracts/DTO/SellOrderRequest.cs
@@ -0,0 +1,23 @@
+﻿using StocksApp.ServiceContracts.CustomValidators;
+using System.ComponentModel.DataAnnotations;
+
+namespace StocksApp.ServiceContracts.DTO
+{
+    public class SellOrderRequest
+    {
+        [Required(ErrorMessage = "StockSymbol can't be empty")]
+        public string? StockSymbol { get; set; }
+
+        [Required(ErrorMessage = "StockName can't be empty")]
+        public string? StockName { get; set; }
+
+        [MinimumDate]
+        public DateTime DateAndTimeOfOrder { get; set; }
+
+        [Range(1, 100000, ErrorMessage = "Quantity must be 1 to 100000")]
+        public uint Quantity { get; set; }
+
+        [Range(1, 10000, ErrorMessage = "Price must be 1 to 10000")]
+        public double Price { get; set; }
+    }
+}
