500877b3848835ddcd6293334d6ebec575576c24
João Felipe Fernandes Pimentel <joaofelipefernandes3221@gmail.com>
2026-01-30 17:32:12 -0300

    feat: Override Equals method to use in tests

--- StocksApp/ServiceContracts/DTO/SellOrderResponse.cs
+++ StocksApp/ServiceContracts/DTO/SellOrderResponse.cs
@@ -1,4 +1,6 @@
-﻿namespace StocksApp.ServiceContracts.DTO
+﻿using StocksApp.Entities;
+
+namespace StocksApp.ServiceContracts.DTO
 {
     public class SellOrderResponse
     {
@@ -9,5 +11,26 @@
         public uint Quantity { get; set; }
         public double Price { get; set; }
         public double TradeAmount { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj == null) return false;
+
+            if (obj.GetType() != typeof(SellOrderResponse)) return false;
+
+            SellOrderResponse sellOrderObject = (SellOrderResponse)obj;
+            return SellOrderID == sellOrderObject.SellOrderID &&
+                   StockSymbol == sellOrderObject.StockSymbol &&
+                   StockName == sellOrderObject.StockName &&
+                   DateAndTimeOfOrder == sellOrderObject.DateAndTimeOfOrder &&
+                   Quantity == sellOrderObject.Quantity &&
+                   Price == sellOrderObject.Price &&
+                   TradeAmount == sellOrderObject.TradeAmount;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
     }
 }
