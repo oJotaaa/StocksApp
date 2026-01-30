a386b96a9698e5d8166a385cffc03587903e6961
João Felipe Fernandes Pimentel <joaofelipefernandes3221@gmail.com>
2026-01-30 16:00:18 -0300

    feat: Add StocksService with CreateBuyOrder implementation logic

---
+++ StocksApp/Services/StocksService.cs
@@ -0,0 +1,55 @@
+﻿using StocksApp.Entities;
+using StocksApp.ServiceContracts;
+using StocksApp.ServiceContracts.DTO;
+using StocksApp.ServiceContracts.Extensions;
+using StocksApp.Services.Helpers;
+
+namespace StocksApp.Services
+{
+    public class StocksService : IStocksService
+    {
+        // Private fields
+        private readonly List<BuyOrder> _buyOrders;
+        private readonly List<SellOrder> _sellOrders;
+
+        // Constructor
+        public StocksService() 
+        {
+            _buyOrders = new List<BuyOrder>();
+            _sellOrders = new List<SellOrder>();
+        }
+
+        public Task<BuyOrderResponse> CreateBuyOrder(BuyOrderRequest? buyOrderRequest)
+        {
+            // Check if buyOrderRequest is null
+            if (buyOrderRequest == null)
+                throw new ArgumentNullException(nameof(buyOrderRequest));
+
+            // Validate all properties of buyOrderRequest
+            ValidationHelper.ModelValidation(buyOrderRequest);
+
+            // Convert buyOrderRequest to BuyOrder Entitie and put a new orderID
+            BuyOrder buyOrder = buyOrderRequest.ToBuyOrder();
+            buyOrder.BuyOrderID = Guid.NewGuid();
+            _buyOrders.Add(buyOrder);
+
+            // Return a buyOrderReponse
+            return Task.FromResult(buyOrder.ToBuyOrderResponse());
+        }
+
+        public Task<SellOrderResponse> CreateSellOrder(SellOrderRequest? sellOrderRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<BuyOrderResponse>> GetBuyOrders()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<SellOrderResponse>> GetSellOrders()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
