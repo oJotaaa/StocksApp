94be192444a9680c18de7fb55c27fc33a69ee942
João Felipe Fernandes Pimentel <joaofelipefernandes3221@gmail.com>
2026-01-30 15:53:58 -0300

    feat: Add a custom validator to calculate MinimumDateAttribute

---
+++ StocksApp/ServiceContracts/CustomValidators/MinimumDateAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StocksApp.ServiceContracts.CustomValidators
+{
+    public class MinimumDateAttribute : ValidationAttribute
+    {
+        override protected ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value is DateTime dateTimeValue)
+            {
+                if (dateTimeValue < new DateTime(2000, 1, 1))
+                {
+                    return new ValidationResult("Date must be on or after January 1, 2000.");
+                }
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
