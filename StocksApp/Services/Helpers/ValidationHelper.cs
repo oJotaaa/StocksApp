3e4b8c0116f0dd0b3459bb539de86a26ed653d9f
João Felipe Fernandes Pimentel <joaofelipefernandes3221@gmail.com>
2026-01-30 15:58:42 -0300

    feat: Add validationHelper to verify all models attributes

---
+++ StocksApp/Services/Helpers/ValidationHelper.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StocksApp.Services.Helpers
+{
+    public class ValidationHelper
+    {
+        internal static void ModelValidation(object obj)
+        {
+            // Validate all properties of "obj"
+            ValidationContext validationContext = new ValidationContext(obj);
+            List<ValidationResult> validationResults = new List<ValidationResult>();
+
+            bool isValid = Validator.TryValidateObject(obj, validationContext, validationResults, true);
+            if (!isValid)
+            {
+                throw new ArgumentException(validationResults.FirstOrDefault()?.ErrorMessage);
+            }
+        }
+    }
+}
