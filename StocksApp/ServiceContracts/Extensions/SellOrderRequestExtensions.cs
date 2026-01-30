c2c19134fe19f3fd58ec73b92480536b201269fa
João Felipe Fernandes Pimentel <joaofelipefernandes3221@gmail.com>
2026-01-30 16:36:23 -0300

    feat: Add ToSellOrder extension method

---
+++ StocksApp/ServiceContracts/Extensions/SellOrderRequestExtensions.cs
@@ -0,0 +1,20 @@
+﻿using StocksApp.Entities;
+using StocksApp.ServiceContracts.DTO;
+
+namespace StocksApp.ServiceContracts.Extensions
+{
+    public static class SellOrderRequestExtensions
+    {
+        public static SellOrder ToSellOrder(this SellOrderRequest sellOrderRequest)
+        {
+            return new SellOrder
+            {
+                StockSymbol = sellOrderRequest.StockSymbol,
+                StockName = sellOrderRequest.StockName,
+                DateAndTimeOfOrder = sellOrderRequest.DateAndTimeOfOrder,
+                Quantity = sellOrderRequest.Quantity,
+                Price = sellOrderRequest.Price
+            };
+        }
+    }
+}
