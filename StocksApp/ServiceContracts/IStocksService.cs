f9c43009304a0a210dcf334d10ef163ccc3f5437
João Felipe Fernandes Pimentel <joaofelipefernandes3221@gmail.com>
2026-01-30 15:57:28 -0300

    feat: Add interfaces with methods to IFinnhubService and IStocksService

---
+++ StocksApp/ServiceContracts/IStocksService.cs
@@ -0,0 +1,12 @@
+﻿using StocksApp.ServiceContracts.DTO;
+
+namespace StocksApp.ServiceContracts
+{
+    public interface IStocksService
+    {
+        Task<BuyOrderResponse> CreateBuyOrder(BuyOrderRequest? buyOrderRequest);
+        Task<SellOrderResponse> CreateSellOrder(SellOrderRequest? sellOrderRequest);
+        Task<List<BuyOrderResponse>> GetBuyOrders();
+        Task<List<SellOrderResponse>> GetSellOrders();
+    }
+}
