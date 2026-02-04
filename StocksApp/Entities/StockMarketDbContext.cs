2cdcfe6f43ee436a799f6d12341d5bc3e6f0b8cc
João Felipe Fernandes Pimentel <joaofelipefernandes3221@gmail.com>
2026-02-04 10:11:27 -0300

    feat: Add StockMarketDbContext to use SQL Server Database

---
+++ StocksApp/Entities/StockMarketDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StocksApp.Entities
+{
+    public class StockMarketDbContext : DbContext
+    {
+        public StockMarketDbContext(DbContextOptions<StockMarketDbContext> options) : base(options) { }
+
+        public DbSet<BuyOrder> BuyOrders { get; set; }
+        public DbSet<SellOrder> SellOrders { get; set; }
+    }
+}
