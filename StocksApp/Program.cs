f1faa6c31df21691731f9ffc6d94edc785aad57f
João Felipe Fernandes Pimentel <joaofelipefernandes3221@gmail.com>
2026-02-04 10:14:40 -0300

    feat: Add DbContext in the Builder Services with configurations to use SQL Server

--- StocksApp/Program.cs
+++ StocksApp/Program.cs
@@ -1,13 +1,19 @@
+using Microsoft.EntityFrameworkCore;
 using StocksApp;
+using StocksApp.Entities;
 using StocksApp.ServiceContracts;
 using StocksApp.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<StockMarketDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 builder.Services.AddControllersWithViews();
 builder.Services.AddHttpClient();
 builder.Services.Configure<TradingOptions>(builder.Configuration.GetSection("TradingOptions"));
 builder.Services.AddScoped<IFinnhubService, FinnhubService>();
-builder.Services.AddSingleton<IStocksService, StocksService>();
+builder.Services.AddScoped<IStocksService, StocksService>();
 
 var app = builder.Build();
 app.UseStaticFiles();
