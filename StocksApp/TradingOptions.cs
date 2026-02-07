95a811a394a23e4ff744eb75e28a3a956cf2602d
João Felipe Fernandes Pimentel <joaofelipefernandes3221@gmail.com>
2026-02-07 20:17:52 -0300

    feat: Add Top25PopularStocks option

--- StocksApp/TradingOptions.cs
+++ StocksApp/TradingOptions.cs
@@ -4,5 +4,6 @@
     {
         public string? DefaultStockSymbol { get; set; }
         public uint DefaultOrderQuantity { get; set; }
+        public string? Top25PopularStocks { get; set; }
     }
 }
