90651bc057da31e479ef00ba7f43c91747cbe7d1
João Felipe Fernandes Pimentel <joaofelipefernandes3221@gmail.com>
2026-02-07 20:05:04 -0300

    feat: Add repository layer to access Database

---
+++ StocksApp/Repositories/FinnhubRepository.cs
@@ -0,0 +1,101 @@
+﻿using StocksApp.RepositoryContracts;
+using System.Text.Json;
+
+namespace StocksApp.Repositories
+{
+    public class FinnhubRepository : IFinnhubRepository
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly IConfiguration _configuration;
+
+        public FinnhubRepository(IHttpClientFactory httpClientFactory, IConfiguration configuration)
+        {
+            _httpClientFactory = httpClientFactory;
+            _configuration = configuration;
+        }
+
+        public async Task<Dictionary<string, object>?> GetCompanyProfile(string stockSymbol)
+        {
+            HttpClient httpClient = _httpClientFactory.CreateClient();
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, $"https://finnhub.io/api/v1/stock/profile2?symbol={stockSymbol}&token={_configuration["apiKey"]}");
+
+            HttpResponseMessage response = await httpClient.SendAsync(request);
+            if (response.IsSuccessStatusCode)
+            {
+                string jsonResponse = await response.Content.ReadAsStringAsync();
+                var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+                var companyProfile = JsonSerializer.Deserialize<Dictionary<string, object>>(jsonResponse, options);
+                return companyProfile;
+            }
+            else
+            {
+                string errorResponse = await response.Content.ReadAsStringAsync();
+                Console.WriteLine($"Error fetching company profile: {errorResponse}");
+                return null;
+            }
+        }
+
+        public async Task<Dictionary<string, object>?> GetStockPriceQuote(string stockSymbol)
+        {
+            HttpClient httpClient = _httpClientFactory.CreateClient();
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, $"https://finnhub.io/api/v1/quote?symbol={stockSymbol}&token={_configuration["apiKey"]}");
+
+            HttpResponseMessage response = await httpClient.SendAsync(request);
+            if (response.IsSuccessStatusCode)
+            {
+                string jsonResponse = await response.Content.ReadAsStringAsync();
+                var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+                var stockQuote = JsonSerializer.Deserialize<Dictionary<string, object>>(jsonResponse, options);
+                return stockQuote;
+            }
+            else
+            {
+                string errorResponse = await response.Content.ReadAsStringAsync();
+                Console.WriteLine($"Error fetching company price quote: {errorResponse}");
+                return null;
+            }
+        }
+
+        public async Task<List<Dictionary<string, string>>?> GetStocks()
+        {
+            HttpClient httpClient = _httpClientFactory.CreateClient();
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, $"https://finnhub.io/api/v1/stock/symbol?exchange=US&token={_configuration["apiKey"]}");
+
+            HttpResponseMessage response = await httpClient.SendAsync(request);
+            if (response.IsSuccessStatusCode)
+            {
+                string jsonResponse = await response.Content.ReadAsStringAsync();
+                var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+                var stocksList = JsonSerializer.Deserialize<List<Dictionary<string, string>>>(jsonResponse, options);
+                return stocksList;
+            }
+            else
+            {
+                string errorResponse = await response.Content.ReadAsStringAsync();
+                Console.WriteLine($"Error fetching stocks: {errorResponse}");
+                return null;
+            }
+        }
+
+        public async Task<Dictionary<string, object>?> SearchStocks(string stockSymbolToSearch)
+        {
+            HttpClient httpClient = _httpClientFactory.CreateClient();
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, $"https://finnhub.io/api/v1/search?q={stockSymbolToSearch}&token={_configuration["apiKey"]}");
+
+            HttpResponseMessage response = await httpClient.SendAsync(request);
+            if (response.IsSuccessStatusCode)
+            {
+                string jsonResponse = await response.Content.ReadAsStringAsync();
+                var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+                var searchResults = JsonSerializer.Deserialize<Dictionary<string, object>>(jsonResponse, options);
+                return searchResults;
+            }
+            else
+            {
+                string errorResponse = await response.Content.ReadAsStringAsync();
+                Console.WriteLine($"Error fetching stocks: {errorResponse}");
+                return null;
+            }
+        }
+    }
+}
---
+++ StocksApp/Repositories/StocksRepository.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StocksApp.Entities;
+using StocksApp.RepositoryContracts;
+
+namespace StocksApp.Repositories
+{
+    public class StocksRepository : IStocksRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public StocksRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<BuyOrder> CreateBuyOrder(BuyOrder buyOrder)
+        {
+            await _db.BuyOrders.AddAsync(buyOrder);
+            await _db.SaveChangesAsync();
+            return buyOrder;
+        }
+
+        public async Task<SellOrder> CreateSellOrder(SellOrder sellOrder)
+        {
+            await _db.SellOrders.AddAsync(sellOrder);
+            await _db.SaveChangesAsync();
+            return sellOrder;
+        }
+
+        public async Task<List<BuyOrder>> GetBuyOrders()
+        {
+            List<BuyOrder> buyOrders = await _db.BuyOrders.ToListAsync();
+            return buyOrders;
+        }
+
+        public async Task<List<SellOrder>> GetSellOrders()
+        {
+            List<SellOrder> sellOrders = await _db.SellOrders.ToListAsync();
+            return sellOrders;
+        }
+    }
+}
