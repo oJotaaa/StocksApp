321229e2c19b1bd0c38d3d9dacfcbe19ed668712
João Felipe Fernandes Pimentel <joaofelipefernandes3221@gmail.com>
2026-02-07 20:09:48 -0300

    feat: Use FinnhubRepository to access data

--- StocksApp/Services/FinnhubService.cs
+++ StocksApp/Services/FinnhubService.cs
@@ -1,59 +1,45 @@
-﻿using StocksApp.ServiceContracts;
+﻿using StocksApp.RepositoryContracts;
+using StocksApp.ServiceContracts;
 using System.Text.Json;
 
 namespace StocksApp.Services
 {
     public class FinnhubService : IFinnhubService
     {
-        private readonly IConfiguration _configuration;
-        private readonly IHttpClientFactory _httpClient;
+        private readonly IFinnhubRepository _finnhubRepository;
 
-        public FinnhubService(IHttpClientFactory httpClientFactory, IConfiguration configuration)
+        public FinnhubService(IFinnhubRepository finnhubRepository)
         {
-            _configuration = configuration;
-            _httpClient = httpClientFactory;
+            _finnhubRepository = finnhubRepository;
         }
 
         public async Task<Dictionary<string, object>?> GetCompanyProfile(string stockSymbol)
         {
-            HttpClient httpClient = _httpClient.CreateClient();
-            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, $"https://finnhub.io/api/v1/stock/profile2?symbol={stockSymbol}&token={_configuration["apiKey"]}");
-
-            HttpResponseMessage response = await httpClient.SendAsync(request);
-            if (response.IsSuccessStatusCode)
-            {
-                string jsonResponse = await response.Content.ReadAsStringAsync();
-                var options = new JsonSerializerOptions{ PropertyNameCaseInsensitive = true };
-                var companyProfile = JsonSerializer.Deserialize<Dictionary<string, object>>(jsonResponse, options);
-                return companyProfile;
-            }
-            else
-            {
-                string errorResponse = await response.Content.ReadAsStringAsync();
-                Console.WriteLine($"Error fetching company profile: {errorResponse}");
+            if (string.IsNullOrEmpty(stockSymbol))
                 return null;
-            }
+
+            return await _finnhubRepository.GetCompanyProfile(stockSymbol);
         }
 
         public async Task<Dictionary<string, object>?> GetStockPriceQuote(string stockSymbol)
         {
-            HttpClient httpClient = _httpClient.CreateClient();
-            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, $"https://finnhub.io/api/v1/quote?symbol={stockSymbol}&token={_configuration["apiKey"]}");
-
-            HttpResponseMessage response = await httpClient.SendAsync(request);
-            if (response.IsSuccessStatusCode)
-            {
-                string jsonResponse = await response.Content.ReadAsStringAsync();
-                var options = new JsonSerializerOptions{ PropertyNameCaseInsensitive = true };
-                var stockData = JsonSerializer.Deserialize<Dictionary<string, object>>(jsonResponse, options);
-                return stockData;
-            }
-            else
-            {
-                string errorResponse = await response.Content.ReadAsStringAsync();
-                Console.WriteLine($"Error fetching stock price quote: {errorResponse}");
+            if (string.IsNullOrEmpty(stockSymbol))
                 return null;
-            }
+
+            return await _finnhubRepository.GetStockPriceQuote(stockSymbol);
+        }
+
+        public async Task<List<Dictionary<string, string>>?> GetStocks()
+        {
+            return await _finnhubRepository.GetStocks();
+        }
+
+        public async Task<Dictionary<string, object>?> SearchStocks(string stockSymbolToSearch)
+        {
+            if (string.IsNullOrEmpty(stockSymbolToSearch))
+                return null;
+
+            return await _finnhubRepository.SearchStocks(stockSymbolToSearch);
         }
     }
 }
