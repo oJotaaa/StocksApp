40f84e799195aaeeef69f39b2353d01209478841
João Felipe Fernandes Pimentel <joaofelipefernandes3221@gmail.com>
2026-02-07 20:17:22 -0300

    feat: Add Repositories to IoC container in builder services

--- StocksApp/Program.cs
+++ StocksApp/Program.cs
@@ -1,11 +1,13 @@
 using Microsoft.EntityFrameworkCore;
 using StocksApp;
 using StocksApp.Entities;
+using StocksApp.Repositories;
+using StocksApp.RepositoryContracts;
 using StocksApp.ServiceContracts;
 using StocksApp.Services;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<StockMarketDbContext>(options =>
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
@@ -14,6 +16,8 @@ builder.Services.AddHttpClient();
 builder.Services.Configure<TradingOptions>(builder.Configuration.GetSection("TradingOptions"));
 builder.Services.AddScoped<IFinnhubService, FinnhubService>();
 builder.Services.AddScoped<IStocksService, StocksService>();
+builder.Services.AddScoped<IFinnhubRepository, FinnhubRepository>();
+builder.Services.AddScoped<IStocksRepository, StocksRepository>();
 
 var app = builder.Build();
 
