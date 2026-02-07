4ddf4cc7d0b2d99b8e9a3924171ccbbfc0ec11f5
João Felipe Fernandes Pimentel <joaofelipefernandes3221@gmail.com>
2026-02-07 20:12:43 -0300

    feat: Add SelectedStockViewComponent to use in Explore view

---
+++ StocksApp/ViewComponents/SelectedStockViewComponent.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StocksApp.ServiceContracts;
+
+namespace StocksApp.ViewComponents
+{
+    public class SelectedStockViewComponent : ViewComponent
+    {
+        private readonly IFinnhubService _finnhubService;
+
+        public SelectedStockViewComponent(IFinnhubService finnhubService)
+        {
+            _finnhubService = finnhubService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string stockSymbol)
+        {
+            Dictionary<string, object>? companyProfile = await _finnhubService.GetCompanyProfile(stockSymbol);
+            Dictionary<string, object>? stockPriceQuote = await _finnhubService.GetStockPriceQuote(stockSymbol);
+
+            if (companyProfile != null && stockPriceQuote != null)
+            {
+                companyProfile["price"] = stockPriceQuote["c"];
+                companyProfile["stockSymbol"] = stockSymbol;
+                return View(companyProfile);
+            }
+
+            return View();
+        }
+    }
+}
