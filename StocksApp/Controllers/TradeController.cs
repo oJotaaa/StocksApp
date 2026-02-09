eb06a2912e935c0f7427051d1e9c4f9317d63912
João Felipe Fernandes Pimentel <joaofelipefernandes3221@gmail.com>
2026-02-09 14:46:08 -0300

    feat: Add logs to each action in TradeController

--- StocksApp/Controllers/TradeController.cs
+++ StocksApp/Controllers/TradeController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using Rotativa.AspNetCore;
+using StocksApp.Entities;
 using StocksApp.ServiceContracts;
 using StocksApp.ServiceContracts.DTO;
 using StocksApp.ViewModels;
@@ -13,15 +14,17 @@ namespace StocksApp.Controllers
     {
         private readonly TradingOptions _tradingOptions;
         private readonly IConfiguration _configuration;
+        private readonly ILogger<TradeController> _logger;
         private readonly IFinnhubService _finnhubService;
         private readonly IStocksService _stocksService;
 
-        public TradeController(IOptions<TradingOptions> tradingOptions, IFinnhubService finnhubService, IConfiguration configuration, IStocksService stocksService)
+        public TradeController(IOptions<TradingOptions> tradingOptions, IFinnhubService finnhubService, IConfiguration configuration, IStocksService stocksService, ILogger<TradeController> logger)
         {
             _tradingOptions = tradingOptions.Value;
             _finnhubService = finnhubService;
             _configuration = configuration;
             _stocksService = stocksService;
+            _logger = logger;
         }
 
         [Route("/")]
@@ -29,6 +32,9 @@ namespace StocksApp.Controllers
         [Route("~/[controller]")]
         public async Task<IActionResult> Index(string? stockSymbol)
         {
+            // Log
+            _logger.LogInformation("Index action called from TradeController with stockSymbol: {stockSymbol}", stockSymbol);
+
             if (string.IsNullOrEmpty(_tradingOptions.DefaultStockSymbol))
                 _tradingOptions.DefaultStockSymbol = "MSFT";
 
@@ -64,6 +70,9 @@ namespace StocksApp.Controllers
         [Route("[action]")]
         public async Task<IActionResult> BuyOrder(BuyOrderRequest buyOrder)
         {
+            // Log
+            _logger.LogInformation("BuyOrder action called from TradeController with buyOrder: {@buyOrder}", buyOrder);
+
             if (!ModelState.IsValid) 
             {
                 ViewBag.ApiKey = _configuration["apiKey"];
@@ -87,6 +96,9 @@ namespace StocksApp.Controllers
         [Route("[action]")]
         public async Task<IActionResult> SellOrder(SellOrderRequest sellOrder)
         {
+            // Log
+            _logger.LogInformation("SellOrder action called from TradeController with sellOrder: {@sellOrder}", sellOrder);
+
             if (!ModelState.IsValid)
             {
                 ViewBag.ApiKey = _configuration["apiKey"];
@@ -110,6 +122,9 @@ namespace StocksApp.Controllers
         [Route("[action]")]
         public async Task<IActionResult> Orders()
         {
+            // Log
+            _logger.LogInformation("Orders action called from TradeController");
+
             List<BuyOrderResponse> buyOrders = await _stocksService.GetBuyOrders();
             List<SellOrderResponse> sellOrders = await _stocksService.GetSellOrders();
             Orders orders = new Orders()
@@ -125,6 +140,9 @@ namespace StocksApp.Controllers
         [Route("[action]")]
         public async Task<IActionResult> OrdersPDF()
         {
+            // Log
+            _logger.LogInformation("OrdersPDF action called from TradeController");
+
             List<BuyOrderResponse> buyOrders = await _stocksService.GetBuyOrders();
             List<SellOrderResponse> sellOrders = await _stocksService.GetSellOrders();
             Orders orders = new Orders()
