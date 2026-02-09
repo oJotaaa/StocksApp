0ceff884475f8ed431b4087a642b391b38648b8f
João Felipe Fernandes Pimentel <joaofelipefernandes3221@gmail.com>
2026-02-09 14:48:26 -0300

    feat: Build Serilog and use AddHttpLogging

--- StocksApp/Program.cs
+++ StocksApp/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Serilog;
 using StocksApp;
 using StocksApp.Entities;
 using StocksApp.Repositories;
@@ -7,10 +8,19 @@ using StocksApp.ServiceContracts;
 using StocksApp.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((HostBuilderContext context, IServiceProvider services, LoggerConfiguration loggerConfiguration) =>
+{
+    loggerConfiguration
+    .ReadFrom.Configuration(context.Configuration)
+    .ReadFrom.Services(services);
+});
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
 builder.Services.AddControllersWithViews();
 builder.Services.AddHttpClient();
 builder.Services.Configure<TradingOptions>(builder.Configuration.GetSection("TradingOptions"));
@@ -19,10 +29,17 @@ builder.Services.AddScoped<IStocksService, StocksService>();
 builder.Services.AddScoped<IFinnhubRepository, FinnhubRepository>();
 builder.Services.AddScoped<IStocksRepository, StocksRepository>();
 
+builder.Services.AddHttpLogging(options =>
+{
+    options.LoggingFields = Microsoft.AspNetCore.HttpLogging.HttpLoggingFields.RequestProperties | Microsoft.AspNetCore.HttpLogging.HttpLoggingFields.ResponseHeaders;
+});
+
 var app = builder.Build();
 
 Rotativa.AspNetCore.RotativaConfiguration.Setup(app.Environment.WebRootPath, "Rotativa");
 
+app.UseSerilogRequestLogging();
+app.UseHttpLogging();
 app.UseStaticFiles();
 app.UseRouting();
 app.MapControllers();
