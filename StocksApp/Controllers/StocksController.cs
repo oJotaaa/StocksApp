63a682cc7d8f1761efd53fcffe6b33739c250a02
João Felipe Fernandes Pimentel <joaofelipefernandes3221@gmail.com>
2026-02-09 14:45:56 -0300

    feat: Add logs to each action in StocksController

--- StocksApp/Controllers/StocksController.cs
+++ StocksApp/Controllers/StocksController.cs
@@ -9,20 +9,24 @@ namespace StocksApp.Controllers
     public class StocksController : Controller
     {
         private readonly TradingOptions _tradingOptions;
+        private readonly ILogger<StocksController> _logger;
         private readonly IFinnhubService _finnhubService;
         private readonly IStocksService _stocksService;
 
-        public StocksController(IOptions<TradingOptions> tradingOptions, IFinnhubService finnhubService, IStocksService stocksService)
+        public StocksController(IOptions<TradingOptions> tradingOptions, IFinnhubService finnhubService, IStocksService stocksService, ILogger<StocksController> logger)
         {
             _tradingOptions = tradingOptions.Value;
             _finnhubService = finnhubService;
             _stocksService = stocksService;
+            _logger = logger;
         }
 
         [HttpGet]
         [Route("[action]/{stock?}")]
         public async Task<IActionResult> Explore(string? stock)
         {
+            _logger.LogInformation("Explore action called from StocksController");
+
             ViewBag.Stock = stock;
             string? topPopularStocksString = _tradingOptions.Top25PopularStocks;
             string[] topPopularStocks = topPopularStocksString!.Split(",");
