103c0c51e3f095f399fe5c51d8f74043c035ab3a
João Felipe Fernandes Pimentel <joaofelipefernandes3221@gmail.com>
2026-02-11 11:20:19 -0300

    feat: Add IOrderRequest interface to BuyOrderRequest and SellOrderRequest use in filters

---
+++ StocksApp/ServiceContracts/IOrderRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace StocksApp.ServiceContracts
+{
+    public interface IOrderRequest
+    {
+        string? StockSymbol { get; set; }
+        string? StockName { get; set; }
+        double Price { get; set; }
+        uint Quantity { get; set; }
+    }
+}
